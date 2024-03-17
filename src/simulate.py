372b80d43473747f56bc6aa19b38b4aeafb590b3
KjennethObrice <kenneth.olibrice@gmail.com>
2024-03-17 08:40:10 -0400

    Successfully reading from bsc5.dat. Currently projecting the entire celestial sphere onto a plane. Next step is to project a small portion of it onto a tangent plane.

---
+++ src/simulate.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+from dataclasses import dataclass
+import pandas as pd
+
+@dataclass(order=True)
+class StarData:
+    name: str
+    right_ascension: float
+    declination: float
+    constellation: str
+    Vmag: float
+
+def parse_float(n):
+    try:
+        return float(n)
+    except:
+        return None
+
+data = []
+with open("../res/bsc5.dat") as catalog:
+    buffer = catalog.readlines()
+    for line in buffer:
+        try:
+            ra = parse_float(line[75:77]) + parse_float(line[77:79]) / 60 + parse_float(line[79:83]) / 3600
+            dec = parse_float(line[84:86]) + parse_float(line[86:88]) / 60 + parse_float(line[88:90]) / 3600
+            data.append(StarData(line[4:14], ra, dec, line[11:14].strip(), parse_float(line[102:107])))
+        except:
+            continue
+
+df = pd.DataFrame(data)
+
+# df.to_csv("../res/star_data.csv")
+
+# Allows you to plot a constellation with ra on the x-axis and dec on the y-axis
+target_constellation = "Peg"  # Edit this to pick a specific constellation
+for star in data:
+    format = 'bo'
+    if star.constellation == target_constellation:
+        format = 'ro'
+
+    plt.plot([star.right_ascension], [star.declination], format)
+
+plt.show()

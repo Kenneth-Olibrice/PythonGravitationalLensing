38028f759aabfa2c2cf9cb8c8d1633cd23a7cac2
KjennethObrice <kenneth.olibrice@gmail.com>
2024-04-10 03:41:07 -0400

    Added some sun bending but it's too minimal to be noticeable.

--- src/Main.py
+++ src/Main.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import math
 from Util import read_data, gnomonic_projection, average
 import matplotlib.pyplot as plt
 
@@ -9,7 +9,9 @@ stars = []
 target_stars = []
 read_data("../res/bsc5.dat", stars)
 df = pd.DataFrame(stars)
-data = [[], [], []]
+data = [[], [], [], [], []]
+
+displacement = (1.75 / 60) / 60
 
 for star in stars:
     if star.constellation == "UMa" and star.name[:6].strip(" ") in star_names:
@@ -20,9 +22,18 @@ avg_dec = average([star.coord.dec.deg for star in target_stars])
 
 for star in target_stars:
     print(f"Name: {star.name[:6].strip(" ")} , {star.coord.ra.deg} , {star.coord.dec.deg} ")
-    x, y = gnomonic_projection(star.coord.ra.deg, star.coord.dec.deg, avg_ra, avg_dec, 1)
+    r = (avg_ra - star.coord.ra.deg, avg_dec - star.coord.dec.deg)
+    length = math.sqrt(math.pow(r[0], 2) + math.pow(r[1], 2))
+    displaced_ra = star.coord.ra.deg + displacement * math.cos(r[0] / length)
+    displaced_dec = star.coord.dec.deg + displacement * math.sin(r[1] / length)
+
+    x1, y1 = gnomonic_projection(star.coord.ra.deg, star.coord.dec.deg, avg_ra, avg_dec, 1)
+    x, y = gnomonic_projection(displaced_ra, displaced_dec, avg_ra, avg_dec, 1)
     data[0].append(x)
     data[1].append(y)
+    data[3].append(x1)
+    data[4].append(y1)
+
     if star.name[:6].strip(" ") in star_names:
         data[2].append(6)
     else:
@@ -30,5 +41,6 @@ for star in target_stars:
 
 
 for i in range(len(data[0])):
-    plt.plot([data[0][i]], [data[1][i]], 'ro' if data[2][i] > 5 else 'bo')
+    plt.plot([data[0][i]], [data[1][i]], 'ro')
+    plt.plot([data[3][i]], [data[4][i]], 'bo')
 plt.show()

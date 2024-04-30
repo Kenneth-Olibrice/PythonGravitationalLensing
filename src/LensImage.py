7ef0873a2914bca1e9b85385c1c964b42f738533
KjennethObrice <kenneth.olibrice@gmail.com>
2024-04-30 04:05:51 -0400

    Pretty much finished now.

--- src/LensImage.py
+++ src/LensImage.py
@@ -1,6 +1,54 @@
 import matplotlib.image as mim
 import matplotlib.pyplot as plt
+import numpy as np
+from scipy import ndimage
+import math
+import scipy
 
-img = mim.imread("../res/hubble_galaxies.jpg")
-plt.imshow(img)
+# Capitalized variables are customizable constants
+RADIUS = 200
+LENS_X = 130
+LENS_Y = 20
+IMG_PATH = "../res/red_galaxy.jpg"
+
+img = mim.imread(IMG_PATH)
+img_height, img_width = [img.shape[1], img.shape[0]] # img size
+
+x_center = img_width / 2
+y_center = img_height / 2
+
+
+def radial_distortion(r, max):
+    if r < max:
+        return (max - r) * math.cos((math.pi * r) / (2 * max))
+    else:
+        return 0
+
+
+def lens(a):
+    x = a[0] - x_center
+    y = a[1] - y_center
+
+    # y is flipped due to +y pointing downwards by default
+    # For our purposes, +x is right and +y is up
+    mag = ((x-LENS_X)**2 + (y+LENS_Y)**2)**0.5
+
+    if mag == 0:
+        return 0, 0, a[2]
+
+    nx = x / mag
+    ny = y / mag
+
+    displacement = radial_distortion(mag, RADIUS)
+    final_x = x + nx * displacement
+    final_y = y + ny * displacement
+
+    return final_x + x_center, final_y + y_center, a[2]
+
+
+arr = np.asarray(img)
+out = scipy.ndimage.geometric_transform(arr, lens)
+
+plt.imshow(out)
 plt.show()
+
